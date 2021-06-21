d076076e35cd7b494a2eae60a0c7b89a5f53e925
Connor Rayburn <connorrayburn.work@gmail.com>
2021-06-21 13:20:56 -0700

    set up store and auth/reducer files

--- potluck-app/src/index.js
+++ potluck-app/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { potluckReducer } from '../src/Reducers/PotluckReducer';
+
+const store = createStore(potluckReducer, applyMiddleware(logger));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
