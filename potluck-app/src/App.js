26e429ba1c67e28ae187be544dbad90527a8511d
Spencerp34 <spencerp34@yahoo.com>
2021-06-24 12:36:31 -0600

    added front page css

--- potluck-app/src/App.js
+++ potluck-app/src/App.js
@@ -15,43 +15,50 @@ function App() {
 			<div className="wrapper">
 				<div className="container">
 					<div className="nav">
-						<div className="logo">Potluck Planning!</div>
-						<div className="menu">
-							<ul className="navMenu">
-									<li>
-										<a href="#0">Home</a>
-									</li>
-                  <li>
-										<a href="#0">Login</a>
-									</li>
-                  <li>
-										<a href="#0">Signup</a>
-									</li>
-              </ul>
+            <div className='header'>
+
+						  <div className="logo">Potluck Planning!</div>
+              <div className="menu">
+                <ul className="navMenu">
+                    <li>
+                      <a href="#0">Home</a>
+                    </li>
+                    <li>
+                      <a href="#0">Login</a>
+                    </li>
+                    <li>
+                      <a href="#0">Signup</a>
+                    </li>
+                </ul>
+              </div>
             </div>
-            <Switch>
-              <Route path="/signup">
-                <Signup />
-              </Route>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route
-                path="/"
-                render={props => {
-                  return (
-                    <div className="header">
-                      <h1>Plan Your Potluck!</h1>
-                      <p>Set up your event and items to bring!</p>
-                      <Link to="/signup">
-                        <button className="button">Get Started</button>
-                      </Link>
-                      {/* <NewEvent /> testing the forms before we connect */}
-                    </div>
-                  );
-                }}
-						  />
-            </Switch>
+            <div className='main-content'>
+              <Switch>
+                <Route path="/signup">
+                  <Signup />
+                </Route>
+                <Route path="/login">
+                  <Login />
+                </Route>
+                <Route
+                  path="/"
+                  render={props => {
+                    return (
+                      <div className="cta">
+                        <div className='cta-pic'>
+                          <h1>Plan Your Potluck!</h1>
+                        </div>
+                        <p>Set up your event and items to bring!</p>
+                        <Link to="/signup">
+                          <button className="button">Get Started</button>
+                        </Link>
+                        {/* <NewEvent /> testing the forms before we connect */}
+                      </div>
+                    );
+                  }}
+                />
+              </Switch>
+            </div>
           </div>   
 
 				</div>
